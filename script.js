8d856a5097e7ccd7af0f0daee990ca11fa318354
Paras-5 <paras.ag94@gmail.com>
2022-08-25 11:48:07 +0530

    removed2

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Fertilisers","FlexFuel",],
+        strings: ["Fertilisers","FlexFuel"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
